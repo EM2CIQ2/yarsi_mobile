2ead283842480dba81f73aadb74a61a3fbd8c44e
Unero <20809454+Unero@users.noreply.github.com>
2020-09-30 14:04:27 +0700

    Experiment ScrollView
    Add ScrollView Activity
    Data Class
    Parcelable

--- app/src/main/java/com/unero/pmbyarsimobile/DataPribadi.kt
+++ app/src/main/java/com/unero/pmbyarsimobile/DataPribadi.kt
@@ -1,18 +1,21 @@
 package com.unero.pmbyarsimobile
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import com.google.android.material.appbar.CollapsingToolbarLayout
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
 
-class DataPribadi : Fragment() {
+class DataPribadi : AppCompatActivity() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_data_pribadi, container, false)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_data_pribadi)
+        setSupportActionBar(findViewById(R.id.toolbar))
+        findViewById<CollapsingToolbarLayout>(R.id.toolbar_layout).title = title
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
     }
 }
--- app/src/main/java/com/unero/pmbyarsimobile/MainActivity.kt
+++ app/src/main/java/com/unero/pmbyarsimobile/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.unero.pmbyarsimobile
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnOne: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        
+        btnOne = findViewById(R.id.btnOne)
+
+        btnOne.setOnClickListener {
+            startActivity(Intent(this, DataPribadi::class.java))
+        }
     }
 }
